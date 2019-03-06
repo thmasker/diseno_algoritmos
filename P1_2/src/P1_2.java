4743bfb1a4d5cb7e4140f5edb5f245a89d7e4a0a
Alberto Velasco Mata <Alberto.Velasco1@alu.uclm.es>
2019-03-06 10:52:33 +0100

    [P3] doc: Added description

--- P1_2/src/P1_2.java
+++ P1_2/src/P1_2.java
@@ -2,6 +2,8 @@ import java.util.Stack;
 
 public class P1_2 {
 	public static final int ITERATIONS = 1000;
+	public static final int N = 20;
+	public static final int K = N/2;
 	
 	static long combinatorio_iterativo(int n, int k) {
 		
@@ -73,7 +75,7 @@ public class P1_2 {
 	}
 
 	public static void main(String[] args) {
-		long sum, elapsed_mean, start;
+		long sum, min, max, mean, start, elapsed;
 		
 		long result = 0;
 		
@@ -81,42 +83,51 @@ public class P1_2 {
 		 * ITERATIVO
 		 */
 		System.out.println("[ ITERATIVO ]");
-		sum = 0;
+		sum = 0; min = Long.MAX_VALUE; max = 0;
 		for(int i = 0; i < ITERATIONS; i++) {
 			start = System.nanoTime();
-			result = combinatorio_iterativo(20,3);
-			sum += System.nanoTime() - start;
+			result = combinatorio_iterativo(N,K);
+			elapsed = System.nanoTime() - start;
+			sum += elapsed;
+			if(elapsed < min) min = elapsed;
+			if(elapsed > max) max = elapsed;
 		}
-		elapsed_mean = sum/ITERATIONS;
-		System.out.printf("Result: %d; Elapsed: %d ns\n", result, elapsed_mean);
+		mean = sum/ITERATIONS;
+		System.out.printf("Result: %d; Elapsed: %d ns (min: %d ns, max: %d ns)\n", result, mean, min, max);
 		
 		
 		/*
 		 * RECURSIVO
 		 */
 		System.out.println("[ RECURSIVO ]");
-		sum = 0;
+		sum = 0; min = Long.MAX_VALUE; max = 0;
 		for(int i = 0; i < ITERATIONS; i++) {
 			start = System.nanoTime();
-			result = combinatorio_recursivo(20,3);
-			sum += System.nanoTime() - start;
+			result = combinatorio_recursivo(N,K);
+			elapsed = System.nanoTime() - start;
+			sum += elapsed;
+			if(elapsed < min) min = elapsed;
+			if(elapsed > max) max = elapsed;
 		}
-		elapsed_mean = sum/ITERATIONS;
-		System.out.printf("Result: %d; Elapsed: %d ns\n", result, elapsed_mean);
+		mean = sum/ITERATIONS;
+		System.out.printf("Result: %d; Elapsed: %d ns (min: %d ns, max: %d ns)\n", result, mean, min, max);
 		
 		
 		/*
 		 * RECURSIVO CON PILAS
 		 */
 		System.out.println("[ RECURSIVO PILAS ]");
-		sum = 0;
+		sum = 0; min = Long.MAX_VALUE; max = 0;
 		for(int i = 0; i < ITERATIONS; i++) {
 			start = System.nanoTime();
-			result = combinatorio_recursivo_pilas(20,3);
-			sum += System.nanoTime() - start;
+			result = combinatorio_recursivo_pilas(N,K);
+			elapsed = System.nanoTime() - start;
+			sum += elapsed;
+			if(elapsed < min) min = elapsed;
+			if(elapsed > max) max = elapsed;
 		}
-		elapsed_mean = sum/ITERATIONS;
-		System.out.printf("Result: %d; Elapsed: %d ns\n", result, elapsed_mean);		
+		mean = sum/ITERATIONS;
+		System.out.printf("Result: %d; Elapsed: %d ns (min: %d ns, max: %d ns)\n", result, mean, min, max);	
 	}
 
 }
