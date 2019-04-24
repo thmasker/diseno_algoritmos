034e5b958ef3b646ec1c832ec7763b9ce7e984f3
thmasker <diego.pedregal@alu.uclm.es>
2019-04-24 14:10:57 +0200

    P5 actualización

--- P5/src/P5.java
+++ P5/src/P5.java
@@ -1,3 +1,15 @@
 public class P5 {
-
+	public static void main(String [] args) {
+		Leer leer = new Leer();
+		
+		System.out.println("-------------	BuscaBusca S.A. (¡encuentra el patrón!)	-------------\n");
+		do {
+			leer.pedirString("Introduce el patrón a buscar:");
+			
+			System.out.println("\n¿Desea buscar más patrones? (y/n)");
+		} while(leer.pedirChar() == 'y');
+		
+		System.out.println("\nHecho por Alberto Velasco Mata y Diego Pedregal Hidalgo, 2019 (C)");
+		System.out.println("Sin derechos reservados :(");
+	}
 }
